41cec782450544e8c3aaf1b172f5226c33af108d
taiyeba-software <islamtaiyeba38@gmail.com>
2025-12-17 21:51:10 +0600

    header completed

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,4 @@
+/*
 import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
@@ -6,4 +7,25 @@ export default authMiddleware({
  
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
+*/
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/api/webhooks/clerk",
+  "/api/webhooks/stripe",
+]);
+
+export default clerkMiddleware(async (auth, req) => {
+  if (!isPublicRoute(req)) {
+    const { userId } = await auth();
+    if (!userId) {
+      return;
+    }
+  }
+});
+
+export const config = {
+  matcher: ["/((?!_next|.*\\..*).*)"],
 };
